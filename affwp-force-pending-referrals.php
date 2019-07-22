5c7c55282fde1286f16d29b651ae3acaae1995f6
Andrew Munro <andrew@amdrew.com>
2019-07-22 13:24:19 +1200

    Update Author and Author URI
    Closes #7

--- affwp-force-pending-referrals.php
+++ affwp-force-pending-referrals.php
@@ -3,8 +3,8 @@
  * Plugin Name: AffiliateWP - Force Pending Referrals
  * Plugin URI: https://affiliatewp.com
  * Description: Forces referrals to be "pending" first
- * Author: AffiliateWP
- * Author URI: https://affiliatewp.com
+ * Author: Sandhills Development, LLC
+ * Author URI: https://sandhillsdev.com
  * Version: 1.0
  * License: GPL-2.0+
  * License URI: http://www.opensource.org/licenses/gpl-license.php
