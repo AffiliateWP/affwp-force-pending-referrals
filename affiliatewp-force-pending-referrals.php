2b9c4e7acf3acff85bbfa56a5edf26030518f3ac
dbeja <david.beja@gmail.com>
2021-06-10 16:13:03 +0100

    Recreate main plugin file. #16

---
+++ affiliatewp-force-pending-referrals.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name: AffiliateWP - Force Pending Referrals
+ * Plugin URI: https://affiliatewp.com
+ * Description: Forces referrals to be "pending" first
+ * Author: Sandhills Development, LLC
+ * Author URI: https://sandhillsdev.com
+ * Version: 1.0
+ * License: GPL-2.0+
+ * License URI: http://www.opensource.org/licenses/gpl-license.php
+ */
