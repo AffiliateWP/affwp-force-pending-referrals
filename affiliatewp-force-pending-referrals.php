8d1040b5551801e41068c0e366723d2502717229
David Beja <david.beja@gmail.com>
2021-06-11 21:29:04 +0100

    issue/15 - Bump the AffiliateWP minimum to 2.6 (#18)
    Fixes #15

--- affiliatewp-force-pending-referrals.php
+++ affiliatewp-force-pending-referrals.php
@@ -37,6 +37,24 @@ class AffiliateWP_FPR_Requirements_Check extends AffiliateWP_Requirements_Check 
 	protected $slug = 'affiliatewp-force-pending-referrals';
 
 	/**
+	 * Add-on requirements.
+	 *
+	 * @since 1.1
+	 * @var   array[]
+	 */
+	protected $addon_requirements = array(
+		// AffiliateWP.
+		'affwp' => array(
+			'minimum' => '2.6',
+			'name'    => 'AffiliateWP',
+			'exists'  => true,
+			'current' => false,
+			'checked' => false,
+			'met'     => false,
+		),
+	);
+
+	/**
 	 * Bootstrap everything.
 	 *
 	 * @since 1.1
